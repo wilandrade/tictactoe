105e9ec5fd02cfce9db3f259657d025d84225d82
Wil A <andradewilker@gmail.com>
2014-10-24 12:09:08 -0700

    Created basic UI layer for board.

--- GameBoardView.js
+++ GameBoardView.js
@@ -1,21 +1,38 @@
 "user strict";
 
 (function (game) {
-  var Tile = function(num){
-    this.name = num;
+  var UI_Tile = function(row, col){
+    this.row = row;
+    this.col = col;
     this.ele = document.createElement('div');
     this.ele.className = 'tile';
-    this.ele.onclick = (function(tileName){
+    this.ele.onclick = (function(curRow, curCol){
       return function(){
-        console.log(tileName + " was clicked.");
+        console.log(curRow +","+curCol+ " was clicked.");
       }
-    }(this.name));
+    }(this.row, this.col));
 
     document.body.appendChild(this.ele);
     
   };
 
-  for(var i=0; i<3; i++){
-    new Tile(i);
-  };
+  var UI_Board = function(){
+    this.tilesVisuals = null;
+    this.model = game;
+    this.initialize();
+  }
+
+  UI_Board.prototype.initialize = function(){
+    this.tileVisuals = [];
+    for(var row=0; row<3; row++){
+          this.tileVisuals.push([]);
+          for(var col=0; col<3; col++){
+              this.tileVisuals[row].push(new UI_Tile(row,col));
+          }
+          this.tileVisuals[row][0].ele.className += " firstTile";
+    }
+  }
+
+  new UI_Board();
+
 }(new GameBoard()));
